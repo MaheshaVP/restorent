fa310bbfedce790c6a7d1b7297bf0e84252f99e6
Mahesha V P <maheshavp12@gmail.com>
2025-11-22 20:03:41 +0530

    React Redux Project

--- src/Store.js
+++ src/Store.js
@@ -1,9 +1,9 @@
 // Store.js
 import { createStore, applyMiddleware } from "redux";
 import {thunk} from "redux-thunk";
-import { composeWithDevTools } from "redux-devtools-extension";
+// import { composeWithDevTools } from "redux-devtools-extension";
 import reducer from "./Reducer";
 
-const store = createStore(reducer, composeWithDevTools(applyMiddleware(thunk)));
+const store = createStore(reducer, (applyMiddleware(thunk)));
 
 export default store;
